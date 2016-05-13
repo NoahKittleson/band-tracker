b0058f387419fd6e9bc1d9be27fa15c7675f2402
Noah Kittleson <noahkittleson@gmail.com>
2016-05-13 09:58:12 -0700

    getName() and all() functions added in Venue

--- src/main/java/Venue.java
+++ src/main/java/Venue.java
@@ -8,4 +8,19 @@ public class Venue {
   public Venue (String name) {
     this.name = name;
   }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public static List<Venue> all() {
+    String sql = "SELECT id, name FROM venues";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Venue.class);
+    }
+  }
 }
--- src/test/java/VenueTest.java
+++ src/test/java/VenueTest.java
@@ -15,4 +15,45 @@ public class VenueTest {
     Venue myVenue = new Venue("Canterbury Ales");
     assertEquals(true, myVenue instanceof Venue);
   }
+
+  @Test
+  public void getName_instantiatesWithName_string() {
+    Venue myVenue = new Venue("The Scarlet Gazelle");
+    assertEquals("The Scarlet Gazelle", myVenue.getName());
+  }
+
+  @Test
+  public void all_emptyAtFirst() {
+    assertEquals(Venue.all().size(), 0);
+  }
+  //
+  // @Test
+  // public void equals_returnsTrueIfNamesAreTheSame_true() {
+  //   Band firstBand = new Band("The Cheatles");
+  //   Band secondBand = new Band("The Cheatles");
+  //   assertTrue(firstBand.equals(secondBand));
+  // }
+  //
+  // @Test
+  // public void save_savesObjectIntoDatabase_true() {
+  //   Band myBand = new Band("King Harkinian's Magical Minions");
+  //   myBand.save();
+  //   assertTrue(Band.all().get(0).equals(myBand));
+  // }
+  //
+  // @Test
+  // public void save_assignsIdToObject() {
+  //   Band myBand = new Band("Methro Skull");
+  //   myBand.save();
+  //   Band savedBand = Band.all().get(0);
+  //   assertEquals(myBand.getId(), savedBand.getId());
+  // }
+  //
+  // @Test
+  // public void find_findsBandsInDatabase_True() {
+  //   Band myBand = new Band("The Antarctic Monkeys");
+  //   myBand.save();
+  //   Band savedBand = Band.find(myBand.getId());
+  //   assertTrue(myBand.equals(savedBand));
+  // }
 }
